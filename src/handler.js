2d19946d3e56d04a028523463fde6175a792f1db
Muhamad Fathul Azis <azisfathl@gmail.com>
2024-03-11 00:03:29 +0700

    adding book api

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,58 @@
+const { nanoid } = require('nanoid');
 const books = require('./books');
 
 const addBookHandler = (request, h) => {
-    // TODO : Ntar
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const id = nanoid(16);
+    const finished = pageCount === readPage;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const isNameValid = name !== undefined;
+    const isPageValid = readPage < pageCount;
+
+    const newBook = { id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt };
+
+    if (!isNameValid) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+
+    if (!isPageValid) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+
+    if (isNameValid && isPageValid) {
+        books.push(newBook);
+        const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+        if (isSuccess) {
+            const response = h.response({
+                status: 'success',
+                message: 'Buku berhasil ditambahkan',
+                data: {
+                    bookId: id,
+                },
+            });
+            response.code(201);
+            return response;
+        }
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
 };
 
 module.exports = {
