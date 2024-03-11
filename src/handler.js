3dcf56e691b6a36bf4eab39f5b390ed81dcc2058
Muhamad Fathul Azis <azisfathl@gmail.com>
2024-03-11 19:28:43 +0700

    get book by id api

--- src/handler.js
+++ src/handler.js
@@ -62,7 +62,29 @@ const getAllBooksHandler = () => {
     return ({status: 'success', data: newBooks });
 };
 
+const getBookByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const book = books.filter((n) => n.id === id)[0];
+
+    if(book !== undefined) {
+        return {
+            status: 'success',
+            data: { book },
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
 module.exports = {
     addBookHandler,
     getAllBooksHandler,
+    getBookByIdHandler,
 };
